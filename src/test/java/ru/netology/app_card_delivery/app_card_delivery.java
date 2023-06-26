265d6b5842e999ba2800b80361cc88cd360a4e74
Karatsupa <andreykaratsupa@gmail.com>
2023-06-26 18:02:48 +0300

    First commit

---
+++ src/test/java/ru/netology/app_card_delivery/app_card_delivery.java
@@ -0,0 +1,23 @@
+package ru.netology.app_card_delivery;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class app_card_delivery {
+    @Test
+    public void CardTest() {
+        open("http://localhost:9999");
+        $("span[data-test-id='city'] input").setValue("Казань");
+        $("span[data-test-id='date'] input").setValue("2023_06_30");
+        $("span[data-test-id='name'] input").setValue("Ким-Чан Ин");
+        $("span[data-test-id='phone'] input").setValue("+79000000000");
+        $("label[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("h2").shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
